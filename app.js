064c629a20e09e8fcfb895ed949f0b4a7e89c935
Gaurprit <84295735+Gaurprit@users.noreply.github.com>
2021-05-17 03:40:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ mongoose.connect(process.env.CONNECTION_STRING,{
 //app.listen(3000);
 
 //Production
-var server=app,listen(process.env.PORT || 3000, function(){
+var server=app.listen(process.env.PORT || 3000, function(){
     var port =server.address().port;
     console.log("Express is Working on port"+port)
 })
